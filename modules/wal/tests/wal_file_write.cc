c9fb813a470cbd6ce4e46ab4268872a079abedb8
Ariane van der Steldt <ariane@stack.nl>
2024-03-20 01:40:11 +0000

    also test non-durable write

--- modules/wal/tests/wal_file_write.cc
+++ modules/wal/tests/wal_file_write.cc
@@ -39,3 +39,41 @@ TEST(write) {
           expected.begin(), expected.end(),
           records.cbegin(), records.cend()));
 }
+
+TEST(non_durable_write) {
+  using ::earnest::detail::wal_record_create_file;
+
+  const earnest::dir testdir = ensure_dir_exists_and_is_empty("non_durable_write");
+  auto [w] = earnest::execution::sync_wait(earnest::detail::wal_file::create(testdir)).value();
+
+  /*
+   * Test: write to wal.
+   */
+  earnest::execution::sync_wait(
+      w->append(
+          earnest::detail::write_records_buffer(
+              std::initializer_list<earnest::detail::wal_file::write_variant_type>{
+                wal_record_create_file{ .file{ "", "foo"} },
+                wal_record_create_file{ .file{ "", "bar"} },
+                wal_record_create_file{ .file{ "", "baz"} },
+              }),
+          nullptr,
+          true));
+
+  /*
+   * Validation.
+   */
+  CHECK(w->entries_empty());
+  CHECK(w->old_entries_empty());
+  CHECK(w->very_old_entries_empty());
+
+  auto expected = std::initializer_list<earnest::detail::wal_file::record_type>{
+    wal_record_create_file{ .file{ "", "foo"} },
+    wal_record_create_file{ .file{ "", "bar"} },
+    wal_record_create_file{ .file{ "", "baz"} },
+  };
+  auto [records] = earnest::execution::sync_wait(w->records()).value();
+  CHECK(std::equal(
+          expected.begin(), expected.end(),
+          records.cbegin(), records.cend()));
+}
